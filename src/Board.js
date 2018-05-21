ba433603bae46e562f2405429cea64b749d36ea1
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-21 15:57:47 +0800

    Create BoardSquare 'smart' component

--- src/Board.js
+++ src/Board.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { DragDropContext } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
-import Square from "./Square";
+import BoardSquare from "./BoardSquare";
 import Knight from "./Knight";
 import { canMoveKnight, moveKnight } from "./Game";
 
@@ -16,20 +16,20 @@ class Board extends Component {
   renderSquare(i) {
     const x = i % 8;
     const y = Math.floor(i / 8);
-    const black = (x + y) % 2 === 1;
-
-    const [knightX, knightY] = this.props.knightPosition;
-    const piece = x === knightX && y === knightY ? <Knight /> : null;
-
     return (
-      <div
-        key={i}
-        style={{ width: "12.5%", height: "12.5%" }}
-        onClick={() => this.handleSquareClick(x, y)}
-      >
-        <Square black={black}>{piece}</Square>
+      <div key={i} style={{ width: "12.5%", height: "12.5%" }}>
+        <BoardSquare x={x} y={y}>
+          {this.renderPiece(x, y)}
+        </BoardSquare>
       </div>
     );
+  }
+
+  renderPiece(x, y) {
+    const [knightX, knightY] = this.props.knightPosition;
+    if (x === knightX && y === knightY) {
+      return <Knight />;
+    }
   }
 
   render() {
