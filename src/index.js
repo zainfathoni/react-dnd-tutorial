23ed821ab8ae90ce2b1e28629a52ac96195f8317
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-21 15:35:04 +0800

    Render Board squares

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import Board from "./Board";
 
 ReactDOM.render(
-  <Board knightPosition={[0, 0]} />,
+  <Board knightPosition={[7, 4]} />,
   document.getElementById("root")
 );
