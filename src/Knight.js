739eecaba24a50f151f77ba6f62c6f0a175ba314
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-21 15:54:33 +0800

    Make Knight a DragSource

--- src/Knight.js
+++ src/Knight.js
@@ -1,7 +1,42 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { ItemTypes } from "./Constants";
+import { DragSource } from "react-dnd";
 
-export default class Knight extends Component {
+const knightSource = {
+  beginDrag(props) {
+    return {};
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  };
+}
+
+class Knight extends Component {
   render() {
-    return <span>♘</span>;
+    const { connectDragSource, isDragging } = this.props;
+    return connectDragSource(
+      <div
+        style={{
+          opacity: isDragging ? 0.5 : 1,
+          fontSize: 25,
+          fontWeight: "bold",
+          cursor: "move"
+        }}
+      >
+        ♘
+      </div>
+    );
   }
 }
+
+Knight.propTypes = {
+  connectDragSource: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired
+};
+
+export default DragSource(ItemTypes.KNIGHT, knightSource, collect)(Knight);
