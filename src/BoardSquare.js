4f8f822c7f0945d302c93081ffb764f65ea35665
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-21 16:01:13 +0800

    Make BoardSquare a DropTarget

--- src/BoardSquare.js
+++ src/BoardSquare.js
@@ -1,17 +1,61 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Square from "./Square";
+import { canMoveKnight, moveKnight } from "./Game";
+import { ItemTypes } from "./Constants";
+import { DropTarget } from "react-dnd";
 
-export default class BoardSquare extends Component {
+const squareTarget = {
+  drop(props, monitor) {
+    moveKnight(props.x, props.y);
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  };
+}
+
+class BoardSquare extends Component {
   render() {
-    const { x, y } = this.props;
+    const { x, y, connectDropTarget, isOver } = this.props;
     const black = (x + y) % 2 === 1;
 
-    return <Square black={black}>{this.props.children}</Square>;
+    return connectDropTarget(
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%"
+        }}
+      >
+        <Square black={black}>{this.props.children}</Square>
+        {isOver && (
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              height: "100%",
+              width: "100%",
+              zIndex: 1,
+              opacity: 0.5,
+              backgroundColor: "yellow"
+            }}
+          />
+        )}
+      </div>
+    );
   }
 }
 
 BoardSquare.propTypes = {
   x: PropTypes.number.isRequired,
-  y: PropTypes.number.isRequired
+  y: PropTypes.number.isRequired,
+  connectDropTarget: PropTypes.func.isRequired,
+  isOver: PropTypes.bool.isRequired
 };
+
+export default DropTarget(ItemTypes.KNIGHT, squareTarget, collect)(BoardSquare);
