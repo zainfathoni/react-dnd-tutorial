faefeb02d799c2b1f9f095a61b49c520883e37e2
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-21 15:43:57 +0800

    Add moveKnight interaction

--- src/Board.js
+++ src/Board.js
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Square from "./Square";
 import Knight from "./Knight";
+import { moveKnight } from "./Game";
 
 export default class Board extends Component {
+  handleSquareClick(toX, toY) {
+    moveKnight(toX, toY);
+  }
+
   renderSquare(i) {
     const x = i % 8;
     const y = Math.floor(i / 8);
@@ -13,7 +18,11 @@ export default class Board extends Component {
     const piece = x === knightX && y === knightY ? <Knight /> : null;
 
     return (
-      <div key={i} style={{ width: "12.5%", height: "12.5%" }}>
+      <div
+        key={i}
+        style={{ width: "12.5%", height: "12.5%" }}
+        onClick={() => this.handleSquareClick(x, y)}
+      >
         <Square black={black}>{piece}</Square>
       </div>
     );
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,20 @@
-export function observe(receive) {
-  setInterval(
-    () =>
-      receive([Math.floor(Math.random() * 8), Math.floor(Math.random() * 8)]),
-    500
-  );
+let knightPosition = [0, 0];
+let observer = null;
+
+function emitChange() {
+  observer(knightPosition);
+}
+
+export function observe(o) {
+  if (observer) {
+    throw new Error("Multiple observers not implemented.");
+  }
+
+  observer = o;
+  emitChange();
+}
+
+export function moveKnight(toX, toY) {
+  knightPosition = [toX, toY];
+  emitChange();
 }
