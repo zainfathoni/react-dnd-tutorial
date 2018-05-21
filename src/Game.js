5a8760377379b24531d64b955292951a9b02041e
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-21 15:41:22 +0800

    Create Game abstraction

---
+++ src/Game.js
@@ -0,0 +1,7 @@
+export function observe(receive) {
+  setInterval(
+    () =>
+      receive([Math.floor(Math.random() * 8), Math.floor(Math.random() * 8)]),
+    500
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Board from "./Board";
+import { observe } from "./Game";
 
-ReactDOM.render(
-  <Board knightPosition={[7, 4]} />,
-  document.getElementById("root")
+const rootEl = document.getElementById("root");
+
+observe(knightPosition =>
+  ReactDOM.render(<Board knightPosition={knightPosition} />, rootEl)
 );
